b88c103f2cee59eaeabe6544401ed962cc131e79
kadir-zeren <k4dzeren@gmail.com>
2024-03-26 13:00:15 +0300

    img

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Content/>
+      <Content />
       <Footer />
     </div>
   );
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,40 @@
-const Contetnt = () => {
+const Content = () => {
+  const parStyle = {
+    fontFamily: "tahome",
+    fontSize: "1.1rem",
+    textAlign: "justify",
+  };
+
+  const imgStyle = {
+    borderRadius: "20px",
+    width: "400px",
+    cursor: "pointer",
+    display: "block",
+    margin: "auto",
+  };
   return (
     <main>
-      <h2>REACT JS</h2>
-      <p>React is JS library</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
-        molestiae?
+      <h2 style={{ color: "white", backgroundColor: "red" }}>REACT JS</h2>
+      <p style={parStyle}>React is JS library</p>
+
+      <img
+        style={imgStyle}
+        src="https://cdn.pixabay.com/photo/2017/05/08/13/15/bird-2295436_640.jpg"
+        alt="bird"
+      />
+      <img
+        style={imgStyle}
+        src="https://cdn.pixabay.com/photo/2015/11/03/10/23/watercolor-1020509_640.jpg"
+        alt=""
+      />
+      <p style={parStyle}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente,
+        animi reiciendis!
       </p>
+
+      <h4 style={parStyle}>Bu bir h4</h4>
     </main>
   );
 };
 
-export default Contetnt;
+export default Content;
