34cec8e6f88043c07552ed9d6d3b201cb00823e1
kadir-zeren <k4dzeren@gmail.com>
2024-03-26 12:35:50 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Footer } from "./Footer";
 import Header from "./Header";
 
 function App() {
+  console.log("deneme");
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
